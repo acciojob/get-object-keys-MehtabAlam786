14e4636bc937e165cac82d773f44efea27ad7b97
MEHTAB ALAM <136914900+MehtabAlam786@users.noreply.github.com>
2026-03-03 19:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 Object.prototype.getKeys = function () {
   return Object.keys(this);
 };
-
-const student = {
-	name: "Saurabh",
-	age: 22,
-	city: "delhi"
-};
-
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-console.log(getKeys(student));
